6762e8befe3613f231c4dfc4d7626bf7ff1eec38
samorai <oleg.samorai@gmail.com>
2016-06-15 16:18:45 +0300

    PHP 7

--- src/Models/Base.php
+++ src/Models/Base.php
@@ -1,9 +1,9 @@
 <?php
 namespace Nebo15\LumenOauth2\Models;
 
-use Jenssegers\Mongodb\Model as Eloquent;
+use Jenssegers\Mongodb\Eloquent\Model;
 
-class Base extends Eloquent
+class Base extends Model
 {
 
 }
